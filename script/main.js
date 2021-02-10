80647cae2ff35ff4db9f11051463898fb3a388c4
bsmjvd <60875304+bsmjvd@users.noreply.github.com>
2021-02-11 03:49:39 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -275,6 +275,24 @@ const animationTimeline = () => {
     tl.restart();
   });
 };
+window.addEventListener('load', function () {
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        var source = audioCtx.createBufferSource();
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'audio-autoplay.wav');
+        xhr.responseType = 'arraybuffer';
+        xhr.addEventListener('load', function (r) {
+            audioCtx.decodeAudioData(
+                    xhr.response, 
+                    function (buffer) {
+                        source.buffer = buffer;
+                        source.connect(audioCtx.destination);
+                        source.loop = false;
+                    });
+            source.start(0);
+        });
+        xhr.send();
+    });
 
 // Import the data to customize and insert them into page
 const fetchData = () => {
